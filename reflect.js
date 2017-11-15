8511573b83c3a7aa339c2fc7f4547f8bc6f31d25
aksjer <aksogut.jerome@gmail.com>
2017-11-15 14:45:38 +0100

    Update reflect.js

--- reflect.js
+++ reflect.js
@@ -1,11 +1,27 @@
-const o = { a: 'a' };
+// const o = { a: 'a' };
+// const handler = {
+//   deleteProperty: function (target, key) {
+//     console.log(`delete property : ${key}`);
+//   }
+// };
+// const p = new Proxy(o, handler);
+// delete p.a;
+var target = {};
+var yay = Object.defineProperty(target, 'foo', { value: 'bar', configurable: false, writable: false });
+var yay2 = Reflect.defineProperty(target, 'foo', { value: 'bar', configurable: false, writable: false });
+// yay  = {foo:'bar'}
+// yay2 = true
 
-const handler = {
-  deleteProperty: function (target, key) {
-    console.log(`delete property : ${key}`);
-  }
+yay = Object.defineProperty(target, 'foo', { value: 'barz', configurable: false, writable: false });
+// VM96:1 Uncaught TypeError: Cannot redefine property: foo
+//     at Function.defineProperty (<anonymous>)
+//     at <anonymous>:1:14
+// so we need to use try/catch
+try{
+  yay = Object.defineProperty(target, 'foo', { value: 'barz', configurable: false, writable: false });
+}catch(err){
+  console.log(err);
+}
 
-};
-
-const p = new Proxy(o, handler);
-delete p.a;
+yay2 = Reflect.defineProperty(target, 'foo', { value: 'barz', configurable: false, writable: false });
+// yay2 = false
