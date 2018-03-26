393bafa7d15c6d5229636e9ff9d0905b2ae6c840
aksjer <aksogut.jerome@gmail.com>
2018-03-26 14:18:09 +0200

    Update sort.js

--- sort.js
+++ sort.js
@@ -32,3 +32,26 @@ debugger
 triABulles(a);
 // triABullesOptimise(a);
 console.log(a);
+
+function pairImpair(arr) {
+  for (let i = 1; i < arr.length; i = i + 2) {
+    for (let j = 1; j < arr.length; j = j + 2) {
+      if (arr[j] > arr[j + 1]) {
+        const t = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = t;
+      }
+    }
+    for (let j = 0; j < arr.length; j = j + 2) {
+      if (arr[j] > arr[j + 1]) {
+        const t = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = t;
+      }
+    }
+  }
+}
+
+const arr = [3, 4, 0, 5, 1, -5];
+pairImpair(arr);
+console.log(arr);
