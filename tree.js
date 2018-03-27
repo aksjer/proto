45487d83f5a6c3ffafa1d7beef4b77572df5f605
aksjer <aksogut.jerome@gmail.com>
2018-03-27 10:52:11 +0200

    Update tree.js

--- tree.js
+++ tree.js
@@ -26,23 +26,42 @@ Tree.prototype.traverseDF = function (callback) {
     for (let i in node.children) recurse(node.children[i]);
     callback(node);
   })(this.root);
-}
+};
 
 Tree.prototype.traverseBF = function (callback) {
   (function recurse(node) {
     callback(node);
     for (let i in node.children) recurse(node.children[i]);
   })(this.root);
-}
+};
 
 Tree.prototype.contains = function (callback, traversal) {
   traversal.call(this, callback);
-}
+};
 
 Tree.prototype.add = function (data, toData, traversal) {
-  
-}
+  const child = new Node(data);
+  const callback = function (node) {
+    if (node.data === toData) child.parent = node;
+  };
+  this.contains(callback, traversal);
+  if (child.parent) child.parent.children.push(child);
+  else throw Error('Cannot add node to a non-existent parent.');
+};
+
+Tree.prototype.remove = function (data, traversal) {
+  let nodeToRemove;
+  const callback = function (node) {
+    if (node.data === data) nodeToRemove = node;
+  }
+  this.contains(callback, traversal);
+  const parent = nodeToRemove.parent;
+  if (parent) parent.children = parent.children.filter(e => e.data !== nodeToRemove.data);
+  else throw Error('Parent does not exist.');
+};
 
 // t.traverseDF(node => console.log(node));
 // t.traverseBF(node => console.log(node));
-t.contains(node => console.log(node), t.traverseBF);
+// t.contains(node => console.log(node), t.traverseBF);
+// t.add('z', 'a', t.traverseBF);
+// t.remove('d', t.traverseBF);
